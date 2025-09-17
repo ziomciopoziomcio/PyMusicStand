4b1da7e772a918b4b73ddfc9e7208185195091e4
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2025-09-17 22:52:02 +0200

    Spelling fix

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 
-from components.clock_controler import clock_controller
+from components.clock_controller import clock_controller
 
 
 class MainScreen(tk.Tk):
