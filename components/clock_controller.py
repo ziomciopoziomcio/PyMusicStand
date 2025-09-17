60f1cfd18d00728ed466f9146d8e9ee412ef245f
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2025-09-17 23:54:28 +0200

    Clock update sync fix

--- components/clock_controller.py
+++ components/clock_controller.py
@@ -5,6 +5,8 @@ import tkinter as tk
 
 class ClockController:
     def __init__(self, get_label_func):
+        self.current_date = time.strftime("%d.%m.%Y")
+        self.current_time = time.strftime("%H:%M:%S")
         self.get_label_func = get_label_func
         self._running = True
         self.thread = threading.Thread(target=self.update_clock, daemon=True)
@@ -12,18 +14,16 @@ class ClockController:
 
     def set_label(self):
         label = self.get_label_func()
-        current_time = time.strftime("%H:%M:%S")
-        current_date = time.strftime("%d.%m.%Y")
-        label.config(text=f"{current_date} {current_time}")
+        label.config(text=f"{self.current_date} {self.current_time}")
 
     def update_clock(self):
         while self._running:
             label = self.get_label_func()
             if label is not None:
-                current_time = time.strftime("%H:%M:%S")
-                current_date = time.strftime("%d.%m.%Y")
+                self.current_time = time.strftime("%H:%M:%S")
+                self.current_date = time.strftime("%d.%m.%Y")
                 try:
-                    label.config(text=f"{current_date} {current_time}")
+                    label.config(text=f"{self.current_date} {self.current_time}")
                 except Exception:
                     raise RuntimeError("Clock label is not available")
             time.sleep(1)
